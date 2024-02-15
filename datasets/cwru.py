5e7858909442f1ae11828456dca58384922f8acf
rochasantos <adriano_angelo@live.com>
2024-02-15 20:56:57 -0300

    Resolvendo conflito

--- datasets/cwru.py
+++ datasets/cwru.py
@@ -249,13 +249,7 @@ class CWRU():
         for train, test in kf.split(self.signal_data):
             yield self.signal_data[train], self.labels[train], self.signal_data[test], self.labels[test]
 
-    def stratifiedkfold(self):
-        if len(self.signal_data) == 0:
-            self.load_acquisitions()
-        kf = StratifiedShuffleSplit(n_splits=self.n_folds, random_state=42)
-        for train, test in kf.split(self.signal_data, self.labels):
-            yield self.signal_data[train], self.labels[train], self.signal_data[test], self.labels[test]
-
+    
     def groupkfold_acquisition(self):
         if len(self.signal_data) == 0:
             self.load_acquisitions()
--- datasets/mfpt.py
+++ datasets/mfpt.py
@@ -183,13 +183,7 @@ class MFPT():
         for train, test in kf.split(self.signal_data):
             yield self.signal_data[train], self.labels[train], self.signal_data[test], self.labels[test]
 
-    def stratifiedkfold(self):
-        if len(self.signal_data) == 0:
-            self.load_acquisitions()
-        kf = StratifiedShuffleSplit(n_splits=self.n_folds)
-        for train, test in kf.split(self.signal_data, self.labels):
-            yield self.signal_data[train], self.labels[train], self.signal_data[test], self.labels[test]
-
+    
     def groupkfold_acquisition(self):
         if len(self.signal_data) == 0:
             self.load_acquisitions()
