db8fdb758d85247af435d03375710f067892502b
rochasantos <adriano_angelo@live.com>
2024-05-10 17:27:02 -0300

    Add constructor to RadomForestClassifier

--- estimators/randomforest.py
+++ estimators/randomforest.py
@@ -6,7 +6,7 @@ from imblearn.under_sampling import RandomUnderSampler
 from estimators.features.heterogeneous import Heterogeneous
 
 class Reshape(BaseEstimator, TransformerMixin):
-  def fit(self, X, y=None):
+  def fit(self, X, y=None):   
     return self
   def transform(self, X, y=None):
     return X.reshape(X.shape[0], X.shape[1]*X.shape[2])
@@ -23,8 +23,16 @@ class RandomForest(BaseEstimator, ClassifierMixin):
                  ('under', RandomUnderSampler()), 
                  ('feature', Heterogeneous()),
                  ('model', model)]
-        self.clf = Pipeline(steps=steps) 
+        self.clf = Pipeline(steps=steps)
     def fit(self, X, y):
         self.clf.fit(X, y)
     def predict(self,X):
         return self.clf.predict(X)
+
+class RandomForestConstructor():
+    def __init__(self, n_estimators=100, max_features=None):
+        self.n_estimators = n_estimators
+        self.max_features = max_features
+        
+    def estimator(self):
+        return RandomForest(n_estimators=self.n_estimators, max_features=self.max_features)
