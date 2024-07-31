cb21c273193de1cefe9077014b357e7eb9cb05c4
Adriano Rocha <adriano_angelo@live.com>
2024-07-30 21:15:20 -0300

    adding target_sr which is the target sampling rate as a parameter in the experiment.

--- experimenter_kfold.py
+++ experimenter_kfold.py
@@ -31,7 +31,7 @@ logging.basicConfig(level=logging.INFO, format='%(message)s', handlers=[
 ])
 
 # Define the k-fold procedure
-def kfold(datasets, clfmaker, repetitions=3):
+def kfold(datasets, clfmaker, repetitions=3, target_sr=12000):
     total = np.array([])
     if isinstance(datasets, Iterable):
         X, y, groups, sr = get_acquisitions(datasets)
@@ -48,17 +48,17 @@ def kfold(datasets, clfmaker, repetitions=3):
         accuracies = []
         kf = StratifiedGroupKFold(n_splits=n_folds) #, shuffle=True)
         init = time.time()        
-        logging.info("X.shape {X.shape}")
+        logging.info(f"X.shape {X.shape}")
         for train_index, test_index in kf.split(X, y, groups):
             Xtr, ytr = X[train_index], y[train_index]
             Xte, yte = X[test_index], y[test_index]
             # resampling
-            Xtr_rs = resample_data(Xtr, orig_sr=sr, target_sr=12000)
-            Xte_rs = resample_data(Xte, orig_sr=sr, target_sr=12000)
+            Xtr_rs = resample_data(Xtr, orig_sr=sr, target_sr=target_sr)
+            Xte_rs = resample_data(Xte, orig_sr=sr, target_sr=target_sr)
             # training and prediction
             clf = clfmaker.get_estimator()
-            train_estimator(clf.fit, Xtr, ytr, groups[train_index])
-            ypr = clf.predict(Xte)
+            train_estimator(clf.fit, Xtr_rs, ytr, groups[train_index])
+            ypr = clf.predict(Xte_rs)
             accuracies.append(accuracy_score(yte, ypr))
             # experiment log
             logging.info(f"fold {len(accuracies)}/{n_folds} accuracy: {accuracies[-1]}")
@@ -79,9 +79,9 @@ def kfold(datasets, clfmaker, repetitions=3):
 debug = True
 datasets = [  # debug mode 
     # CWRU(cache_file = "cache/cwru_FE.npy"),
-    CWRU(cache_file = "cache/cwru_DE.npy"),
-    # CWRU(cache_file = "cache/cwru_FE_DE.npy"),
-    # Hust(cache_file="cache/hust_all.npy")
+    # CWRU(cache_file = "cache/cwru_DE.npy"),
+    # CWRU(cache_file = "cache/cwru_FE_DE.npy"),    
+    Hust(cache_file="cache/hust_all.npy")    
 ] if debug else [
     CWRU(config='all'),
     Hust(config='all'),
@@ -95,9 +95,12 @@ datasets = [  # debug mode
 factory = EstimatorFactory()
 factory.set_estimator('random_forest')
 
+# target sampling rate
+target_sr = 51200
+
 # Define and run the experimenter
-def experimenter(datasets=datasets, clfmaker=factory, repetitions=1):
-    kfold(datasets, clfmaker=clfmaker, repetitions=repetitions)
+def experimenter(datasets=datasets, clfmaker=factory, repetitions=1, target_sr=target_sr):
+    kfold(datasets, clfmaker=clfmaker, repetitions=repetitions, target_sr=target_sr)
 
 # Run the experimenter
 if __name__ == "__main__":
