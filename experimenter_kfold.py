2e3055416a43030984f14152b8ae730b2812f1a9
Adriano Rocha <adriano_angelo@live.com>
2024-08-06 13:44:29 -0300

    add f1-score

--- experimenter_kfold.py
+++ experimenter_kfold.py
@@ -8,7 +8,7 @@ from datasets.phm import PHM
 from utils.train_estimator import train_estimator
 from utils.get_acquisitions import get_acquisitions
 from collections.abc import Iterable
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from sklearn.model_selection import StratifiedGroupKFold, cross_val_score
 import numpy as np
 import time
@@ -43,9 +43,11 @@ def kfold(datasets, clfmaker, repetitions=3, target_sr=12000):
         n_folds = datasets.n_folds
         logging.info(datasets)
     logging.info('-----------------------------------------')
+    total_accuracies = []
     for i in range(repetitions):
         logging.info(f"{i+1}/{repetitions}: ")
         accuracies = []
+        f1_scores = []
         kf = StratifiedGroupKFold(n_splits=n_folds) #, shuffle=True)
         init = time.time()        
         logging.info(f"X.shape {X.shape}")
@@ -60,6 +62,7 @@ def kfold(datasets, clfmaker, repetitions=3, target_sr=12000):
             Xte_rs = resample_data(Xte, orig_sr=orig_sr, target_sr=target_sr)
             ypr = clf.predict(Xte_rs)
             accuracies.append(accuracy_score(yte, ypr))
+            f1_scores.append(f1_score(yte, ypr, average='macro', labels=['N', 'I', 'O', 'B']))
             # experiment log
             logging.info(f"fold {len(accuracies)}/{n_folds} accuracy: {accuracies[-1]}")
             labels = list(set(yte))
@@ -68,11 +71,15 @@ def kfold(datasets, clfmaker, repetitions=3, target_sr=12000):
         final = time.time()
         logging.info(f'Processing time: {final-init}')
         mean_accuracy = sum(accuracies)/len(accuracies)
+        mean_f1_score = sum(f1_scores)/len(f1_scores)
         logging.info(f"mean accuracy: {mean_accuracy}")
+        logging.info(f"mean f1-score: {mean_f1_score}")
         total = np.append(total, mean_accuracy)
+        total_accuracies.extend(accuracies)
     logging.info('-----------------------------------------')
     logging.info(f"Total Mean Accuracy: {np.mean(total)}")
     logging.info(f"Standard Deviation: {np.std(total)}")
+    logging.info(f"Total Accuracies:\n {total_accuracies}")
     logging.info('-----------------------------------------')
 
 # Set the debug mode and define the datasets
@@ -80,7 +87,11 @@ debug = True
 datasets = [  # debug mode 
     # CWRU(cache_file = "cache/cwru_FE.npy"),
     # CWRU(cache_file = "cache/cwru_DE.npy"),
-    CWRU(cache_file = "cache/cwru_DE_FE.npy"),    
+    # CWRU(cache_file = "cache/cwru_FEDE.npy"),    
+    CWRU(cache_file = "cache/cwru_48k.npy"),    
+        
+    # CWRU(cache_file = "cache/cwru_48k.npy"),    
+    # Hust(cache_file="cache/hust_B5.npy")
     # Hust(cache_file="cache/hust_all.npy")    
 ] if debug else [
     CWRU(config='all'),
@@ -98,7 +109,7 @@ factory.set_estimator('random_forest')
 # SAMPLING RATES
 # hust -> 51200
 # cwru -> 12000/48000
-target_sr = 12000
+target_sr = 48000
 
 # Define and run the experimenter
 def experimenter(datasets=datasets, clfmaker=factory, repetitions=1, target_sr=target_sr):
