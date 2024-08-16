1b9fab99b3f30186921268f0053a5fb209e5cf01
Adriano Rocha <adriano_angelo@live.com>
2024-08-16 11:05:16 -0300

    add split_acquisition function

--- utils/get_acquisitions.py
+++ utils/get_acquisitions.py
@@ -3,15 +3,23 @@ import numpy as np
 def get_acquisitions(datasets):
     first_dataset = True
     for dataset in datasets:
-        # print(dataset)
-        Xtmp, ytmp, gtmp, srtmp = dataset.get_acquisitions()
+        Xtmp, ytmp, gtmp = dataset.get_acquisitions()
         if first_dataset:
-            X, y, g, sr =  Xtmp, ytmp, gtmp, srtmp
+            X, y, g =  Xtmp, ytmp, gtmp
             first_dataset = False
         else:
             gtmp += np.max(gtmp)
             X = np.concatenate((X, Xtmp))
             y = np.concatenate((y, ytmp))
             g = np.concatenate((g, gtmp))
-            sr = np.concatenate((sr, srtmp))
-    return X, y, g, sr
+    return X, y, g
+
+
+def split_acquisition(acquisition, sample_size):
+    acquisitions = np.empty((0, sample_size, 1))
+    for i in range(acquisition.shape[1]//sample_size):
+        sample = acquisition[:,(i * sample_size):((i + 1) * sample_size)]
+        sample = sample[:, :, np.newaxis] # add a dimension to channel
+        acquisitions = np.append(acquisitions, sample, axis=0)
+    return acquisitions
+    
