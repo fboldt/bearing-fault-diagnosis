9f1fa0619dae08e751448d5e2f00e29bd58598e6
Adriano Rocha <adriano_angelo@live.com>
2024-08-16 11:07:16 -0300

    add test experimenter_kfold

---
+++ test/test_experimenter_kfold.py
@@ -0,0 +1,19 @@
+from datasets.cwru import CWRU
+from experimenter_kfold import kfold
+from estimators.estimator_factory import EstimatorFactory
+
+class TestExperimenterKFold():
+
+    """
+    Testing kfold function
+    """
+    def test_kfold(self):
+        fac = EstimatorFactory()
+        fac.set_estimator('random_forest')
+        kfold(CWRU(config='12k'), clfmaker=fac, repetitions=1)
+
+    """
+    Execute the code
+    """
+    def to_test(self):
+        self.test_kfold()
