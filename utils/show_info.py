2951559fb417b24a417e42c4dfd0e0d13c64deee
Adriano Rocha <adriano_angelo@live.com>
2024-08-16 10:28:18 -0300

    remove unused

--- utils/show_info.py
+++
@@ -1,7 +0,0 @@
-def show_title(title):
-    title_length = len(title)
-    width = title_length + 2
-
-    print(" ","-" * width)
-    print(f"| {title.center(title_length + 2)} |")
-    print(" ","-" * width)
