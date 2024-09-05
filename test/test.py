dd7d24882455faa28ac28e049576e5fc062b6f58
Adriano Rocha <adriano_angelo@live.com>
2024-09-05 20:27:44 -0300

    add test paderborn

--- test/test.py
+++ test/test.py
@@ -2,7 +2,6 @@ from test_cwru import TestCWRU
 from test_utils import TestUtils
 from test_ottawa import TestOttawa
 from test_uored import TestUORED
-from test_experimenter_kfold import TestExperimenterKFold
 
 if __name__ == "__main__":
     tests = [
@@ -10,7 +9,6 @@ if __name__ == "__main__":
         TestCWRU(),
         TestUORED(),
         TestUtils(),
-        TestExperimenterKFold(),
     ]
     
     for test in tests:
